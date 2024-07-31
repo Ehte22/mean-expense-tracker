bd8df07df1c17bf56a6312d78ce407b6d776d20a
Ehte22 <syedehteshamali22@gmail.com>
2024-07-31 15:14:21 +0530

    frontend added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const path = require("path")
 const cookieParser = require("cookie-parser")
 const { protectedRoute } = require("./utils/protected")
 require("dotenv").config({ path: "./.env" })
